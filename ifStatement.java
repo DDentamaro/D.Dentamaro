5f563485678e5a7f6e95331589c5f72d86b6d2d2
DD <dentamarodonato99@gmail.com>
2023-12-04 11:15:46 +0100

    checkNumber

---
+++ ifStatement.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ifStatement {
+    public static int checkNumber(int number) {
+        if (number > 10) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Get user input for the number
+        System.out.print("Enter a number: ");
+        int userNumber = scanner.nextInt();
+
+        // Call the checkNumber function
+        int result = checkNumber(userNumber);
+
+        // Print the result
+        System.out.println("Result: " + result);
+
+        scanner.close();
+    }
+}
