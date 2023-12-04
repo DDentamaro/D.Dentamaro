6ab5b49edfed3b0419fec97c602a363895fe54de
DD <dentamarodonato99@gmail.com>
2023-12-04 11:02:09 +0100

    loop

---
+++ loop.java
@@ -0,0 +1,17 @@
+public class loop {
+    public void Loop(){
+
+        for (int i = 0; i <= 10; i++) {
+
+            System.out.println(i);
+        }
+
+    }
+    public static void main(String[] args) {
+        // Create an instance of the Loop class
+        loop loop = new loop();
+
+        // Call the loop method
+        loop.Loop();
+    }
+}
